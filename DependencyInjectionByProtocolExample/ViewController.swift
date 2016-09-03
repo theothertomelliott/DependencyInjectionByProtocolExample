7d665980fe565be8223b4a3c21cdfc5deb416b8e
Tom Elliott <tom.w.elliott@gmail.com>
2016-09-03 15:56:18 -0400

    Initial Commit

---
+++ DependencyInjectionByProtocolExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DependencyInjectionByProtocolExample
+//
+//  Created by Tom Elliott on 9/3/16.
+//  Copyright © 2016 Tom Elliott. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
